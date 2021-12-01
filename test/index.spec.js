a764fb8b183de0b10b3ec2507c77cb6bfc3e8fda
G9D4 <gldt2494@gmail.com>
2021-12-01 10:50:59 -0500

    de spa a src

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,5 @@
 // importamos la funcion que vamos a testear
-import { myFunction } from '../src/lib/index';
+import { myFunction } from '../src/app/index.js';
 
 describe('myFunction', () => {
   it('debería ser una función', () => {
