a021606f09fc6dee720ed8fea48152b7ca55d1c1
mirianalejandra1996 <mirianalejandra1996@gmail.com>
2021-12-02 13:02:10 -0500

    botones sin a tag en vista login

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,6 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { App, ChangeWindow } from "./App.js";
+import { App } from "./App.js";
 
 document.addEventListener("DOMContentLoaded", App);
 window.addEventListener("hashchange", App);
