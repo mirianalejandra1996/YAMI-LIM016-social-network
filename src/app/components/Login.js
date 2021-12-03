31c3f18fdcaf5c7c6bef456ea3be7739340cf98b
mirianalejandra1996 <mirianalejandra1996@gmail.com>
2021-12-03 09:41:55 -0500

    anadido el eslogan de vista login

--- src/app/components/Login.js
+++ src/app/components/Login.js
@@ -3,135 +3,137 @@ export const Login = () => {
   const $login = document.createElement("div");
   $login.classList.add("login");
 
-  const $logoContainer = document.createElement("div")
-  $logoContainer.classList.add("logo-container")
-  const $logo = document.createElement("div")
-  $logo.classList.add("logo")
-  const $logoName = document.createElement("h3")
-  $logoName.classList.add("logo-name")
-  $logoName.textContent= `Yami`
+  const $logoContainer = document.createElement("div");
+  $logoContainer.classList.add("logo-container");
+  const $logo = document.createElement("div");
+  $logo.classList.add("logo");
+  const $logoName = document.createElement("h3");
+  $logoName.classList.add("logo-name");
+  $logoName.textContent = `Yami`;
 
-  $logoContainer.append($logo)
-  $logoContainer.append($logoName)
+  $logoContainer.append($logo);
+  $logoContainer.append($logoName);
 
-  const $eslogan = document.createElement("div")
-  $eslogan.classList.add("eslogan")
-  const $esloganText = document.createElement("h2")
-  $esloganText.textContent=`¿Listo para una nueva aventura culinaria?`
+  const $eslogan = document.createElement("div");
+  $eslogan.classList.add("eslogan");
+  const $esloganText = document.createElement("h2");
+  $esloganText.textContent = `¿Listo para una nueva aventura culinaria?`;
 
-  $eslogan.append($esloganText)
-  
-  const $inputsContainer = document.createElement("div")
-  $inputsContainer.classList.add("inputs-container")
+  $eslogan.append($esloganText);
 
-  const $form = document.createElement("form")
-  $form.classList.add("form")
+  const $inputsContainer = document.createElement("div");
+  $inputsContainer.classList.add("inputs-container");
 
-  const $formInputContainerFirst = document.createElement("div")
-  $formInputContainerFirst.classList.add("form__input-container")
-  const $iconMail = document.createElement("span")
-  $iconMail.classList.add("icon-mail")
-  const $formInputFirst = document.createElement("input")
-  $formInputFirst.classList.add("form__input")
-  $formInputFirst.type="email"
-  $formInputFirst.id="name"
-  $formInputFirst.name="lname"
-  $formInputFirst.placeholder="Correo electrónico"
+  const $form = document.createElement("form");
+  $form.classList.add("form");
 
-  $formInputContainerFirst.append($iconMail)
-  $formInputContainerFirst.append($formInputFirst)
+  const $formInputContainerFirst = document.createElement("div");
+  $formInputContainerFirst.classList.add("form__input-container");
+  const $iconMail = document.createElement("span");
+  $iconMail.classList.add("icon-mail");
+  const $formInputFirst = document.createElement("input");
+  $formInputFirst.classList.add("form__input");
+  $formInputFirst.type = "email";
+  $formInputFirst.id = "name";
+  $formInputFirst.name = "lname";
+  $formInputFirst.placeholder = "Correo electrónico";
 
-  const $formInputContainerSecond = document.createElement("div")
-  $formInputContainerSecond.classList.add("form__input-container")
-  const $iconLock = document.createElement("span")
-  $iconLock.classList.add("icon-lock")
-  const $formInputSecond = document.createElement("input")
-  $formInputSecond.classList.add("form__input")
-  $formInputSecond.type="password"
-  $formInputSecond.id="fname"
-  $formInputSecond.name="fname"
-  $formInputSecond.placeholder="Contraseña"
-  
-  $formInputContainerSecond.append($iconLock)
-  $formInputContainerSecond.append($formInputSecond)
+  $formInputContainerFirst.append($iconMail);
+  $formInputContainerFirst.append($formInputFirst);
 
-  $form.append($formInputContainerFirst)
-  $form.append($formInputContainerSecond)
+  const $formInputContainerSecond = document.createElement("div");
+  $formInputContainerSecond.classList.add("form__input-container");
+  const $iconLock = document.createElement("span");
+  $iconLock.classList.add("icon-lock");
+  const $formInputSecond = document.createElement("input");
+  $formInputSecond.classList.add("form__input");
+  $formInputSecond.type = "password";
+  $formInputSecond.id = "fname";
+  $formInputSecond.name = "fname";
+  $formInputSecond.placeholder = "Contraseña";
 
-  document.createElement("br")
+  $formInputContainerSecond.append($iconLock);
+  $formInputContainerSecond.append($formInputSecond);
 
-  const $forgotPsw = document.createElement("span")
-  $forgotPsw.classList.add("link")
-  $forgotPsw.textContent = `Olvidé mi contraseña`
+  $form.append($formInputContainerFirst);
+  $form.append($formInputContainerSecond);
 
-  document.createElement("br")
+  document.createElement("br");
 
-  const $btn = document.createElement("div")
-  $btn.classList.add("btn")
-  $btn.href="/#timeline"
-  const $ingresar = document.createElement("span")
-  $ingresar.textContent=`Iniciar sesión`
+  const $forgotPsw = document.createElement("span");
+  $forgotPsw.classList.add("link");
+  $forgotPsw.textContent = `Olvidé mi contraseña`;
 
-  $btn.append($ingresar)
+  document.createElement("br");
 
-  document.createElement("br")
+  const $btn = document.createElement("div");
+  $btn.classList.add("btn");
+  $btn.href = "/#timeline";
+  const $ingresar = document.createElement("span");
+  $ingresar.textContent = `Iniciar sesión`;
 
-  const $btnG = document.createElement("div")
-  $btnG.classList.add("btn-g")
-  $btn.href="/#nada"
-  const $googleIcon = document.createElement("span")
-  $googleIcon.classList.add("icon-google")
+  $btn.append($ingresar);
 
-  const $span1 = document.createElement("span")
-  $span1.classList.add("path1")
-  const $span2 = document.createElement("span")
-  $span2.classList.add("path2")
-  const $span3 = document.createElement("span")
-  $span3.classList.add("path3")
-  const $span4 = document.createElement("span")
-  $span4.classList.add("path4")
-  const $span5 = document.createElement("span")
-  $span5.classList.add("path5")
-  const $span6 = document.createElement("span")
-  $span6.classList.add("path6")
+  document.createElement("br");
 
-  $googleIcon.append($span1)
-  $googleIcon.append($span2)
-  $googleIcon.append($span3)
-  $googleIcon.append($span4)
-  $googleIcon.append($span5)
-  $googleIcon.append($span6)
+  const $btnG = document.createElement("div");
+  $btnG.classList.add("btn-g");
+  $btn.href = "/#nada";
+  const $googleIcon = document.createElement("span");
+  $googleIcon.classList.add("icon-google");
 
-  const $google = document.createElement("span")
-  $google.textContent=`Iniciar con Google`
+  const $span1 = document.createElement("span");
+  $span1.classList.add("path1");
+  const $span2 = document.createElement("span");
+  $span2.classList.add("path2");
+  const $span3 = document.createElement("span");
+  $span3.classList.add("path3");
+  const $span4 = document.createElement("span");
+  $span4.classList.add("path4");
+  const $span5 = document.createElement("span");
+  $span5.classList.add("path5");
+  const $span6 = document.createElement("span");
+  $span6.classList.add("path6");
 
-  $btnG.append($googleIcon)
-  $btnG.append($google)
+  $googleIcon.append($span1);
+  $googleIcon.append($span2);
+  $googleIcon.append($span3);
+  $googleIcon.append($span4);
+  $googleIcon.append($span5);
+  $googleIcon.append($span6);
 
-  document.createElement("br")
+  const $google = document.createElement("span");
+  $google.textContent = `Iniciar con Google`;
 
-  const $divLink = document.createElement("div")
-  const $spanAsk = document.createElement("span")
-  $spanAsk.textContent=`¿No tienes una cuenta?`
-  const $link = document.createElement("span")
-  $link.id="sign-up"
-  $link.classList.add("link")
-  $link.textContent=`Regístrate`
-  $link.addEventListener('click', () =>{
-    window.location.hash='#/register'})
+  $btnG.append($googleIcon);
+  $btnG.append($google);
 
-  $divLink.append($spanAsk)
-  $divLink.append($link)
+  document.createElement("br");
 
-  $inputsContainer.append($form)
-  $inputsContainer.append($forgotPsw)
-  $inputsContainer.append($btn)
-  $inputsContainer.append($btnG)
-  $inputsContainer.append($divLink)
+  const $divLink = document.createElement("div");
+  const $spanAsk = document.createElement("span");
+  $spanAsk.textContent = `¿No tienes una cuenta?`;
+  const $link = document.createElement("span");
+  $link.id = "sign-up";
+  $link.classList.add("link");
+  $link.textContent = `Regístrate`;
+  $link.addEventListener("click", () => {
+    window.location.hash = "#/register";
+  });
 
-  $login.append($logo)
-  $login.append($logoContainer)
-  $login.append($inputsContainer)
+  $divLink.append($spanAsk);
+  $divLink.append($link);
 
-  return $login
-}
+  $inputsContainer.append($form);
+  $inputsContainer.append($forgotPsw);
+  $inputsContainer.append($btn);
+  $inputsContainer.append($btnG);
+  $inputsContainer.append($divLink);
+
+  $login.append($logo);
+  $login.append($logoContainer);
+  $login.append($eslogan);
+  $login.append($inputsContainer);
+
+  return $login;
+};
