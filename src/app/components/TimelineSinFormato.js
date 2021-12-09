ee2869156b88aa2af7b37520c020d452d0121809
mirianalejandra1996 <mirianalejandra1996@gmail.com>
2021-12-09 11:27:22 -0500

    guarda el timeline viejo para no perder el boton logout

---
+++ src/app/components/TimelineSinFormato.js
@@ -0,0 +1,31 @@
+import { logOutGoogle } from "../firebase/firebase-auth.js";
+
+export const Timeline = () => {
+  const $timelinePrueba = document.createElement("div");
+
+  const $logoContainer = document.createElement("div");
+  $logoContainer.classList.add("logo-container");
+  $timelinePrueba.append($logoContainer);
+
+  const $iconLogo = document.createElement("div");
+  $iconLogo.classList.add("logo");
+
+  const $logoName = document.createElement("h3");
+  $logoName.textContent = "Yami";
+
+  $logoContainer.append($iconLogo);
+  $logoContainer.append($logoName);
+
+  const $linkContainer = document.createElement("div");
+  $timelinePrueba.append($linkContainer);
+
+  const $botonPrueba = document.createElement("span");
+  $botonPrueba.classList.add("link");
+  $botonPrueba.textContent = "Sign Out";
+
+  $botonPrueba.addEventListener("click", logOutGoogle);
+
+  $linkContainer.append($botonPrueba);
+
+  return $timelinePrueba;
+};
