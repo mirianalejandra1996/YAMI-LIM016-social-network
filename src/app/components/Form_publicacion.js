64c98138497815a5b0b059b665dcc3881c168bd3
mirianalejandra1996 <mirianalejandra1996@gmail.com>
2021-12-09 21:11:00 -0500

    arregla problema de un solo boton de componente form publicacion

--- src/app/components/Form_publicacion.js
+++ src/app/components/Form_publicacion.js
@@ -1,86 +1,92 @@
-import { HeaderRetro } from "./Header_retro.js"
+import { HeaderRetro } from "./Header_retro.js";
 
 export const Form_Post = () => {
-    const root= document.getElementById("root")
-    root.classList.remove("main-container")
+  const root = document.getElementById("root");
+  root.classList.remove("main-container");
 
-    const formPost = document.createElement("div");
-    formPost.classList.add("formPost")
+  // ------NUEVO
 
-    const header = HeaderRetro()
-    formPost.append(header)
+  const createPostContainer = document.createElement("div");
 
-    const title = document.createElement("h2")
-    title.classList.add("formPost_h2")
-    title.textContent = `Crear publicación`
-    formPost.append(title)
+  const formPost = document.createElement("div");
+  formPost.classList.add("formPost");
 
-    const inputsContainer = document.createElement("div")
-    inputsContainer.classList.add("formPost_inputs")
-    formPost.append(inputsContainer)
+  const header = HeaderRetro();
+  formPost.append(header);
 
-    const post = document.createElement("textarea")
-    post.classList.add("formPost_input-long")
-    post.placeholder = `¿Qué estas pensando?`
-    inputsContainer.append(post)
+  const title = document.createElement("h2");
+  title.classList.add("formPost_h2");
+  title.textContent = `Crear publicación`;
+  formPost.append(title);
 
-    const tags = document.createElement("input")
-    tags.classList.add("formPost_input-short")
-    tags.placeholder = `Añadir etiquetas`
-    inputsContainer.append(tags)
+  const inputsContainer = document.createElement("div");
+  inputsContainer.classList.add("formPost_inputs");
+  formPost.append(inputsContainer);
 
-    const picture = document.createElement("input")
-    picture.classList.add("formPost_input-short")
-    picture.placeholder = `Añadir imagen`
-    inputsContainer.append(picture)
+  const post = document.createElement("textarea");
+  post.classList.add("formPost_input-long");
+  post.placeholder = `¿Qué estas pensando?`;
+  inputsContainer.append(post);
 
-    const btnsContainer = document.createElement("div")
-    btnsContainer.classList.add("formPost_btns")
-    formPost.append(btnsContainer)
-    ///////////////////////////////////////////////
+  const tags = document.createElement("input");
+  tags.classList.add("formPost_input-short");
+  tags.placeholder = `Añadir etiquetas`;
+  inputsContainer.append(tags);
 
-    const tagBtn = document.createElement("button")
-    tagBtn.classList.add("formPost_button")
-    btnsContainer.append(tagBtn)
+  const picture = document.createElement("input");
+  picture.classList.add("formPost_input-short");
+  picture.placeholder = `Añadir imagen`;
+  inputsContainer.append(picture);
 
-    const tagBtnDiv = document.createElement("div")
-    tagBtnDiv.classList.add("btnContent")
-    tagBtn.append(tagBtnDiv)
+  const btnsContainer = document.createElement("div");
+  btnsContainer.classList.add("formPost_btns");
+  formPost.append(btnsContainer);
+  ///////////////////////////////////////////////
 
-    const iconTag = document.createElement("span")
-    iconTag.classList.add("icon-plus2")
-    // iconTag.classList.add("icon-acept-icon")
-    // iconTag.classList.add("path1")
-    // iconTag.classList.add("path2")
-    // iconTag.classList.add("path3")
+  const tagBtn = document.createElement("button");
+  tagBtn.classList.add("formPost_button");
+  btnsContainer.append(tagBtn);
 
-    iconTag.classList.add("btnIconsTag")
-    tagBtnDiv.append(iconTag)
+  const tagBtnDiv = document.createElement("div");
+  tagBtnDiv.classList.add("btnContent");
+  tagBtn.append(tagBtnDiv);
 
-    const textTag = document.createElement("span")
-    textTag.classList.add("tagTextSpan")
-    textTag.textContent=`Etiquetas`
-    tagBtnDiv.append(textTag)
+  const iconTag = document.createElement("span");
+  iconTag.classList.add("icon-plus2");
+  // iconTag.classList.add("icon-acept-icon");
+  // iconTag.classList.add("path1");
+  // iconTag.classList.add("path2");
+  // iconTag.classList.add("path3");
 
-    ///////////////////////////////////////////
+  iconTag.classList.add("btnIconsTag");
+  tagBtnDiv.append(iconTag);
 
-    const postBtn = document.createElement("button")
-    postBtn.classList.add("formPost_button")
-    btnsContainer.append(postBtn)
+  const textTag = document.createElement("span");
+  textTag.classList.add("tagTextSpan");
+  textTag.textContent = `Etiquetas`;
+  tagBtnDiv.append(textTag);
 
-    const postBtnDiv = document.createElement("div")
-    postBtnDiv.classList.add("btnContent")
-    postBtn.append(postBtnDiv)
+  ///////////////////////////////////////////
 
-    const iconSend = document.createElement("span")
-    iconSend.classList.add("icon-send")
-    iconSend.classList.add("btnIconsPost")
-    postBtnDiv.append(iconSend)
+  const postBtn = document.createElement("button");
+  postBtn.classList.add("formPost_button");
+  btnsContainer.append(postBtn);
 
-    const textPost = document.createElement("span")
-    textPost.classList.add("postTextSpan")
-    textPost.textContent = `Publicar`
-    postBtnDiv.append(textPost)
+  const postBtnDiv = document.createElement("div");
+  postBtnDiv.classList.add("btnContent");
+  postBtn.append(postBtnDiv);
 
-    return formPost;
+  const iconSend = document.createElement("span");
+  iconSend.classList.add("icon-send");
+  iconSend.classList.add("btnIconsPost");
+  postBtnDiv.append(iconSend);
+
+  const textPost = document.createElement("span");
+  textPost.classList.add("postTextSpan");
+  textPost.textContent = `Publicar`;
+  postBtnDiv.append(textPost);
+
+  createPostContainer.append(formPost);
+
+  return formPost;
 };
