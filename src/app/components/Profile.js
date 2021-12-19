aa592a03284587e405278565c1376ee8cb2569e5
mirianalejandra1996 <mirianalejandra1996@gmail.com>
2021-12-18 22:35:34 -0500

    crea componente de profile en js hardcodeado

--- src/app/components/Profile.js
+++ src/app/components/Profile.js
@@ -33,10 +33,8 @@ export const Profile = () => {
   // Icono para editar imagen del usuario
   const iconPhotoContainer = document.createElement("div");
   iconPhotoContainer.classList.add("photo__edit-img");
-  // TODO: Modificar este iconPhoto, en vez de innerHTML hay que hacer un append del icono del lapiz
   const iconPhoto = document.createElement("span");
-  iconPhoto.innerHTML = "o";
-  //todo: iconPhoto.classList.add('')
+  iconPhoto.classList.add('icon-pencil','pencil')
 
   iconPhotoContainer.append(iconPhoto);
   imgAvatarContainer.append(iconPhotoContainer);
@@ -104,80 +102,108 @@ export const Profile = () => {
 
   // -----------------------------
 
+  // * Grupo: Correo del usuario
+  const groupEmail = document.createElement("div");
+  groupEmail.classList.add("formProfile__group");
+
+  //   Input email
+  const inputEmail = document.createElement("input");
+  inputEmail.type = "email";
+  inputEmail.id = "email";
+  inputEmail.classList.add("formProfile__input");
+  // inputName.placeholder = "Ingresa un nombre"
+  inputEmail.value = "maria_fernandez@gmail.com";
+  inputEmail.disabled = true;
+
+  //   Label de email
+  const labelEmail = document.createElement("label");
+  labelEmail.for = "email";
+  labelEmail.classList.add("formProfile__label");
+  labelEmail.textContent = "Correo";
+
+  //  Email Obligatorio
+  const requiredEmail = document.createElement("span");
+  requiredEmail.classList.add("formProfile__required");
+  requiredEmail.textContent = "*";
+
+  groupEmail.append(inputEmail);
+  groupEmail.append(labelEmail);
+  groupEmail.append(requiredEmail);
+
   // -----------------------------
 
+  // * Grupo: Correo del usuario
+  const groupPwd = document.createElement("div");
+  groupPwd.classList.add("formProfile__group");
+
+  //   Input email
+  const inputPwd = document.createElement("input");
+  inputPwd.type = "password";
+  inputPwd.id = "password";
+  inputPwd.classList.add("formProfile__input");
+  // inputPwd.placeholder = "Ingresa un nombre"
+  inputPwd.value = "laboratoria";
+  inputPwd.disabled = true;
+
+  //   Label de email
+  const labelPwd = document.createElement("label");
+  labelPwd.for = "password";
+  labelPwd.classList.add("formProfile__label");
+  labelPwd.textContent = "Contraseña";
+
+  //  Email Obligatorio
+  const requiredPwd = document.createElement("span");
+  requiredPwd.classList.add("formProfile__required");
+  requiredPwd.textContent = "*";
+
+  const iconPwd = document.createElement("span");
+  iconPwd.classList.add("formProfile__icon", "icon-open-eye");
+
+  groupPwd.append(inputPwd);
+  groupPwd.append(labelPwd);
+  groupPwd.append(requiredPwd);
+  groupPwd.append(iconPwd);
+  // -----------------------------
+
+  //   Contenedor de campos obligatorios
+  const errContainer = document.createElement("div");
+  errContainer.classList.add("errContainer");
+
+  const msgErr = document.createElement("span");
+  msgErr.classList.add("error-msg");
+  msgErr.textContent = "Campos obligatorios *";
+
+  errContainer.append(msgErr);
+  // -----------------------------
+
+  const btnEdit = document.createElement("input");
+  btnEdit.type = "submit";
+  btnEdit.classList.add("formProfile__submit");
+  btnEdit.value = "Editar";
+
+  // -----------------------------
+
+  // Aquí apendizamos todos los datos del usuario
   formContainer.append(groupName);
   formContainer.append(groupDate);
-  mainContainer.append(profileContainer);
-
-  profileContainer.append(photoContainer);
-  profileContainer.append(formContainer);
+  formContainer.append(groupEmail);
+  formContainer.append(groupPwd);
+  //   Apendizamos el mensaje de error
+  formContainer.append(errContainer);
+  //  El botón de Editar
+  formContainer.append(btnEdit);
 
   // -----------------------------
 
   profileComponent.append(headerBack);
   profileComponent.append(mainContainer);
 
-  const prueba = ` 
-  <div class="main-container__profile">
+  mainContainer.append(profileContainer);
 
-      <!-- Datos del usuario -->
-      <form class="formProfile__container">
-        <!-- input -->
-        <div class="formProfile__group">
-         
-          <label for="name" class="formProfile__label">Nombre</label>
-          <span class="formProfile__required">*</span>
-        </div>
-        <!-- input -->
-        <div class="formProfile__group">
-          <input
-            type="date"
-            id="date"
-            class="formProfile__input"
-            placeholder=" "
-          />
-          <label for="date" class="formProfile__label">Fecha de Nacimiento</label>
-          <!-- <span class="formProfile__required">*</span> -->
-        </div>
-        <!-- input -->
-        <div class="formProfile__group">
-          <input
-            type="email"
-            id="email"
-            class="formProfile__input"
-            placeholder=" "
-          />
-          <label for="email" class="formProfile__label">Correo</label>
-          <span class="formProfile__required">*</span>
-        </div>
-        <!-- input -->
-        <div class="formProfile__group">
-          <input
-            type="password"
-            id="password"
-            class="formProfile__input"
-            placeholder=" "
-          />
-          <label for="password" class="formProfile__label">Contraseña</label>
-          <span class="formProfile__required">*</span>
-          <!-- ! Importante, no logro juntar el icono del ojo -->
-          <span class="formProfile__icon icon-open-eye"></span>
-        </div>
-
-        <div class="errContainer">
-          <!-- <span class="error-msg">Campos obligatorios *</span> -->
-        </div>
-        <!-- Input submit -->
-        <input type="submit" class="formProfile__submit" value="Editar">
-      </form>
-        
-      </div>
-    </div>
-  </div>`;
+  profileContainer.append(photoContainer);
+  profileContainer.append(formContainer);
 
   return profileComponent;
-  //   return mainContainer;
 };
 
 // <!-- Cabecera -->
