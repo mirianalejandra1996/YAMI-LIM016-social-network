3c48b078362ac50a1df2f2b60d61d07c09a655a6
G9D4 <gldt2494@gmail.com>
2021-12-19 23:10:29 -0500

    agrega boton de publicar

--- src/app/components/Post-comments.js
+++ src/app/components/Post-comments.js
@@ -13,10 +13,28 @@ export function PostComments() {
     avatarContainer.append(avatarImage)
 
     const inputComment = document.createElement("textarea")
+    inputComment.placeholder = `Escribe un comentario...`
     inputComment.classList.add("postComment_input")
+
+    const commentBtn = document.createElement("div")
+    commentBtn.classList.add("commentBtnDiv")
+    const commentIcon = document.createElement("span")
+    commentIcon.classList.add("icon-send");
+    commentIcon.classList.add("iconComment")
+
+    commentBtn.append(commentIcon)
+
+    // inputComment.addEventListener("keyup", () => {
+    //     let key = window.event.keycode
+    //     if (key === 13/* && inputComment.value.length>0*/){
+    //         alert ("yay")
+    //         // funciondecomentar()
+    //     }
+    // })
 
     newComment.append(avatarContainer)
     newComment.append(inputComment)
+    newComment.append(commentBtn)
 
     return newComment
 }
