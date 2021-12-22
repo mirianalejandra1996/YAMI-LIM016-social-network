1318ed773f2b3221d93d5877ad7091194f1bf7be
mirianalejandra1996 <mirianalejandra1996@gmail.com>
2021-12-22 10:18:20 -0500

    agrega evento de cerrar modal para cancelar edicion de perfil del usuario

--- src/app/components/ModalEditProfile.js
+++ src/app/components/ModalEditProfile.js
@@ -1,174 +1,30 @@
 // import { ModalEditProfile } from "../components/ModalEditProfile.js";
 
-// export const ModalEditProfile = () => {
-//   // * modalContenedor es el overlay
-//   const $modalContenedor = document.createElement("div");
-//   $modalContenedor.classList.add("modal__contenedor");
-//   $modalContenedor.classList.add("modal-cerrar");
-//   //Contenedor de name
-//   const $formPost = document.createElement("div");
-//   $formPost.classList.add("formPost");
-//   // * Cabecera
-//   const $header = document.createElement("div");
-//   $header.classList.add("modal__cabecera");
-//   //Opciones de Cabecera
-//   const $opcionesCabecera = document.createElement("div");
-//   $opcionesCabecera.classList.add("modal__opcionesCabecera");
-//   //Opcion Cerrar
-//   const $cerrarContainer = document.createElement("div");
-//   $cerrarContainer.classList.add("card__icon-container");
-//   $cerrarContainer.addEventListener("click", () => {
-//     cerrarModal();
-//   });
-//   const $iconClose = document.createElement("span");
-//   $iconClose.classList.add("card__icon", "close__icon");
-//   $iconClose.classList.add("icon-icon-close");
-//   $cerrarContainer.appendChild($iconClose);
-
-//   $opcionesCabecera.append($cerrarContainer);
-
-//   //Titulo del Modal
-//   const $title = document.createElement("h2");
-//   $title.classList.add("formPost_h2");
-//   $title.textContent = `Edita tu perfil`;
-
-//   $header.append($opcionesCabecera);
-//   $header.append($title);
-
-//   //Input del Post
-//   const $inputsContainer = document.createElement("div");
-//   $inputsContainer.classList.add("formPost_inputs");
-
-//   const $name = document.createElement("input");
-//   $name.id = "name";
-//   $name.classList.add("formPost_input-short");
-//   $name.placeholder = "Nombre";
-
-//   //   const $date = document.createElement("input");
-//   //   $date.classList.add("formPost_input-short");
-//   //   $date.placeholder = "000000";
-//   //   $date.type = "password";
-//   //   $date.id = "date";
-
-//   const $picture = document.createElement("input");
-//   $picture.type = "file";
-//   $picture.classList.add("formPost_input-short");
-//   $picture.placeholder = `Añadir imagen`;
-//   // picture.onchange = {pictureHandler}
-
-//   $inputsContainer.append($name);
-//   // $inputsContainer.append(date);
-//   $inputsContainer.append($picture);
-
-//   // Contenedor del mensaje de Error
-//   const $errorContainer = document.createElement("div");
-//   $errorContainer.classList.add("err-container", "center");
-
-//   const $msgError = document.createElement("small");
-//   $msgError.classList.add("error-msg");
-//   $msgError.id = "errorCrearPost";
-//   $msgError.textContent = "";
-//   $errorContainer.append($msgError);
-
-//   //Contenedor de Botones
-//   const $btnsContainer = document.createElement("div");
-//   $btnsContainer.classList.add("formPost_btns");
-
-//   //////////////////////////////Boton de Tags
-//   const $tagBtn = document.createElement("button");
-//   $tagBtn.classList.add("formPost_button");
-
-//   const $tagBtnDiv = document.createElement("div");
-//   $tagBtnDiv.classList.add("btnContent");
-//   $tagBtn.append($tagBtnDiv);
-
-//   const $iconTag = document.createElement("span");
-//   $iconTag.classList.add("icon-plus2");
-
-//   $iconTag.classList.add("btnIconsTag");
-//   $tagBtnDiv.append($iconTag);
-
-//   const $textTag = document.createElement("span");
-//   $textTag.classList.add("tagTextSpan");
-//   $textTag.textContent = `Etiquetas`;
-//   $tagBtnDiv.append($textTag);
-
-//   //////////////////////////////////////Boton Publicacion
-
-//   const $postBtn = document.createElement("button");
-//   $postBtn.id = "sendPost";
-//   $postBtn.classList.add("formPost_button");
-
-//   //$btnsContainer.append($tagBtn);
-//   $btnsContainer.append($postBtn);
-
-//   // Anade funcionalidad al boton Publicar y valida campos vacios
-//   $postBtn.addEventListener("click", () => {
-//     const $mensajeError = document.getElementById("errorCrearPost");
-//     const $formPostMsg = document.getElementById("msgPostForm").value;
-//     if ($formPostMsg == "") {
-//       $mensajeError.textContent = "completar campos *";
-//     } else {
-//       console.log("creamos el nuevo name!!", $formPostMsg);
-//       //   addPost($formPostMsg).then(() => {
-//       //     console.log("modal cerrado");
-//       //     cerrarModal();
-//       //   });
-//     }
-//   });
-
-//   const $postBtnDiv = document.createElement("div");
-//   $postBtnDiv.classList.add("btnContent");
-//   $postBtn.append($postBtnDiv);
-
-//   const $iconSend = document.createElement("span");
-//   $iconSend.classList.add("icon-send");
-//   $iconSend.classList.add("btnIconsPost");
-//   $postBtnDiv.append($iconSend);
-
-//   const $textPost = document.createElement("span");
-//   $textPost.classList.add("postTextSpan");
-//   $textPost.textContent = `Publicar`;
-//   $postBtnDiv.append($textPost);
-
-//   $formPost.append($header);
-//   $formPost.append($inputsContainer);
-//   $formPost.append($errorContainer);
-//   $formPost.append($btnsContainer);
-
-//   $modalContenedor.append($formPost);
-//   //Modal oculto
-//   $modalContenedor.style.opacity = "0";
-//   $modalContenedor.style.visibility = "hidden";
-
-//   const abrirModal = () => {
-//     $modalContenedor.style.opacity = "1";
-//     $modalContenedor.style.visibility = "visible";
-//     $modalContenedor.classList.toggle("modal-cerrar");
-//   };
-//   const cerrarModal = () => {
-//     $modalContenedor.classList.toggle("modal-cerrar");
-//     $modalContenedor.style.opacity = "0";
-//     $modalContenedor.style.visibility = "hidden";
-//   };
-
-//   return {
-//     $modalEditProfile: $modalContenedor,
-//     abrirModalEditProfile: abrirModal,
-//     cerrarModalEditProfile: cerrarModal,
-//   };
-// };
-
 export const ModalEditProfile = () => {
   const $modalContenedor = document.createElement("div");
   $modalContenedor.classList.add("modal__contenedor");
   $modalContenedor.classList.add("modal-cerrar");
 
   const profileContainer = document.createElement("div");
-  // profileContainer.classList.add("profile-container");
   profileContainer.classList.add("modal-profile");
-  // profileContainer.classList.add("formPost");
   $modalContenedor.append(profileContainer);
+
+  // Cabecera del modal
+
+  const headerModal = document.createElement("div");
+  headerModal.classList.add("modal-profile__header");
+
+  const titulo = document.createElement("h2");
+  titulo.classList.add("modal-profile__title");
+  titulo.textContent = "Edita tu perfil";
+
+  const closeIcon = document.createElement("span");
+  closeIcon.classList.add("icon-icon-close", "modal-profile__closeIcon");
+  // profileContainer.append(headerModal)
+
+  headerModal.append(titulo);
+  headerModal.append(closeIcon);
+
   //   Contenedor Base de foto del usuario
   const photoContainer = document.createElement("div");
   photoContainer.classList.add("photo__container");
@@ -210,22 +66,15 @@ export const ModalEditProfile = () => {
   const inputName = document.createElement("input");
   inputName.type = "text";
   inputName.id = "name";
-  inputName.classList.add("formProfile__input");
-  inputName.disabled = true;
-
-  //   Label de nombre
-  const labelName = document.createElement("label");
-  labelName.for = "name";
-  labelName.classList.add("formProfile__label");
-  labelName.textContent = "Nombre";
+  inputName.classList.add("modal-profile__input");
+  inputName.placeholder = "Nombre";
 
   //  Nombre Obligatorio
   const requiredName = document.createElement("span");
-  requiredName.classList.add("formProfile__required", "hidden");
+  requiredName.classList.add("formProfile__required");
   requiredName.textContent = "*";
 
   groupName.append(inputName);
-  groupName.append(labelName);
   groupName.append(requiredName);
 
   // -----------------------------
@@ -365,6 +214,7 @@ export const ModalEditProfile = () => {
   // profileComponent.append(headerBack);
   // profileComponent.append(mainContainer);
   // mainContainer.append(profileContainer);
+  profileContainer.append(headerModal);
   profileContainer.append(photoContainer);
   profileContainer.append(formContainer);
   // profileComponent.append($modalEditProfile);
@@ -427,11 +277,13 @@ export const ModalEditProfile = () => {
     $modalContenedor.style.visibility = "hidden";
   };
 
+  closeIcon.addEventListener("click", () => {
+    cerrarModal();
+  });
+
   return {
     $modalEditProfile: $modalContenedor,
     abrirModalEditProfile: abrirModal,
     cerrarModalEditProfile: cerrarModal,
   };
-  // return profileComponent;
-  // return profileContainer;
 };
