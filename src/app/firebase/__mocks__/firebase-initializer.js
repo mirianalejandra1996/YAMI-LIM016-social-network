beccc4fad374adb56fe9c7771af1e23bfea6c3ce
G9D4 <gldt2494@gmail.com>
2022-01-12 10:58:32 -0500

    agrega intento de prueba bienvenida

--- src/app/firebase/__mocks__/firebase-initializer.js
+++ src/app/firebase/__mocks__/firebase-initializer.js
@@ -7,19 +7,28 @@
  const auth = {
    currentUser: {
      uid: newLocal,
+     displayName: "fake-name"
     },
   };
+
+  const userRef = {
+    user: newLocal,
+  }
 
   const enviarIngreso  = jest.fn((auth,email)=>Promise.resolve({values}))
   const loginGoogle  = jest.fn((auth,email)=>Promise.resolve({values}))
   const sendPasswordResetEmail = jest.fn(() => Promise.resolve());
   const GoogleAuthProvider = jest.fn(() => Promise.resolve({}));
-const collection = jest.fn((db, values) => Promise.resolve(values));
- const setDoc = jest.fn((document, values) => Promise.resolve({values}));
- const doc = jest.fn(() => Promise.resolve({}));
- const db = {};
- const app = {};
- const storage = {};
+  const collection = jest.fn((db, values) => Promise.resolve(values));
+  const getDoc = jest.fn((userRef) => Promise.resolve(
+    {values:
+      {name: "fake-name",
+      src: "fake-src"}}))
+  const setDoc = jest.fn((document, values) => Promise.resolve({values}));
+  const doc = jest.fn(() => Promise.resolve({}));
+  const db = {};
+  const app = {};
+  const storage = {};
 
 
 
@@ -30,6 +39,7 @@ export {
   enviarIngreso,
   loginGoogle,
   sendPasswordResetEmail,
+  getDoc,
   setDoc,
   doc,
   db,
