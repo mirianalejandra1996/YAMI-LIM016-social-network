fe9504615b2da5040858ccc8344136b3dee4102b
Lucero <lucerogoga63@hotmail.com>
2022-01-13 16:02:45 -0500

    muestra error en handlesumit then undefined

--- test/login.spec.js
+++ test/login.spec.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { olvideContrasena, Login } from '../src/app/components/Login';
+import { olvideContrasena, Login , handleSubmit} from '../src/app/components/Login';
 
 jest.mock('../src/app/firebase/firebase-auth.js');
 jest.mock('../src/app/firebase/firebase-initializer.js');
@@ -26,23 +26,23 @@ describe('Testing DOM manipulation', () => {
   })
 
 
-//   describe('Testing DOM manipulation of handleSubmit', () => {
-//     beforeAll((done) => {
-//       document.body.id = 'root'
-//       document.body.innerHTML = ""
-//       document.body.append(Login())
-//         done(); 
-//     }),
+  describe('Testing DOM manipulation of handleSubmit', () => {
+    beforeAll((done) => {
+      document.body.id = 'root'
+      document.body.innerHTML = ""
+      document.body.append(Login())
+        done(); 
+    }),
     
-//   it('should return an error code when value is XXXX' , async () => {
-//     const testEmail = document.getElementById('lemail')
-//     const testPassword = document.getElementById('lpassword')
-//     testEmail.value = 'laboratoria@gmail.com'
-//     testPassword.value = 'labolabo'
+  it('should return an error code when value is XXXX' , async () => {
+    const testEmail = document.getElementById('lemail')
+    const testPassword = document.getElementById('lpassword')
+    testEmail.value = 'laboratoria@gmail.com'
+    testPassword.value = 'labolabo'
     
-//     await handleSubmit(testEmail, testPassword)
-//     const $errorLoginMessage  = document.getElementById('errorLogin')
-//     expect($errorLoginMessage).not.toBeUndefined()
-//     expect($errorLoginMessage.textContent).toBe(`Se envió un mensaje al correo ${testEmail}`)
-// });
-//   })
+    await handleSubmit(testEmail, testPassword)
+    const $errorLoginMessage  = document.getElementById('errorLogin')
+    expect($errorLoginMessage).not.toBeUndefined()
+    expect($errorLoginMessage.textContent).toBe(`Se envió un mensaje al correo ${testEmail.value}`)
+});
+  })
